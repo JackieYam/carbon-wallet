1cd17bd922a5c2162ef6f19353bd8262f2cfe7fc
DeNightmare <minecraftgamers824@gmail.com>
2025-04-13 22:24:17 +0800

    update user model and register login

--- carbon_wallet_flask/routes/login.py
+++ carbon_wallet_flask/routes/login.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify
+from werkzeug.security import check_password_hash
+from carbon_wallet_flask.models.user import User
 
 login = Blueprint('login', __name__)
 
@@ -10,10 +12,17 @@ def login_endpoint():
 @login.route('/login-check', methods=['POST'])
 def login_check():
    data = request.get_json()
-   username = data.get('username')
+   email = data.get('email')
    password = data.get('password')
 
-   if username == 'admin' and password == 'password':
-      return jsonify(success=True)
-   else:
-      return jsonify(success=False)
+   if not email or not password:
+      return jsonify(success=False, error="請輸入電子郵件和密碼！")
+
+   user = User.query.filter_by(email=email).first()
+   if not user:
+      return jsonify(success=False, error="帳號或密碼錯誤！")
+
+   if not check_password_hash(user.password, password):
+      return jsonify(success=False, error="帳號或密碼錯誤！")
+
+   return jsonify(success=True)
